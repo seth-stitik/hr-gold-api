59114831f457c22154ea9a710e1fc31e3154a701
seth-stitik <sstitik@gmail.com>
2024-05-13 20:09:47 -0300

    Fix module import paths and case sensitivity issues

--- models/GolfClub.js
+++ models/GolfClub.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const { sequelize } = require('../config/database');
-const Course = require('../Course');
+const Course = require('./Course');
 
 const GolfClub = sequelize.define('golf_club', {
     name: {
