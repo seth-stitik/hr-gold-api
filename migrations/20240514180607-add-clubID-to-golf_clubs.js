28965174259b63609538cce6220b3044d839718a
seth-stitik <sstitik@gmail.com>
2024-05-14 15:08:25 -0300

    redeploy

--- migrations/20240514180607-add-clubID-to-golf_clubs.js
+++ migrations/20240514180607-add-clubID-to-golf_clubs.js
@@ -1,22 +1,15 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('golf_clubs', 'clubID', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('golf_clubs', 'clubID');
   }
 };
--- models/GolfClub.js
+++ models/GolfClub.js
@@ -1,63 +1,52 @@
-module.exports = (sequelize, DataTypes) => {
-    const GolfClub = sequelize.define('GolfClub', {
-      name: {
-        type: DataTypes.STRING,
-        primaryKey: true,
-        allowNull: false
-      },
-      clubName: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      city: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      state: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      country: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      zipCode: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      timeStampUpdated: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-      },
-      distance: {
-        type: DataTypes.FLOAT,
-        allowNull: false
-      },
-      measureUnit: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      clubID: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-      }
-    });
-  
-    GolfClub.associate = (models) => {
-      GolfClub.hasMany(models.Course, { foreignKey: 'golfClubName' });
-    };
-  
-    return GolfClub;
-  };
-  
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const GolfClub = sequelize.define('GolfClub', {
+  name: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    allowNull: false
+  },
+  clubID: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  clubName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  city: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  state: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  country: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  zipCode: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  timeStampUpdated: {
+    type: DataTypes.BIGINT,
+    allowNull: false
+  },
+  distance: {
+    type: DataTypes.FLOAT,
+    allowNull: false
+  },
+  measureUnit: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'golf_clubs',
+  timestamps: true
+});
+
+module.exports = GolfClub;
