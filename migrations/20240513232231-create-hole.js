c98706a495e57d238a85551af856bb67d52d420b
seth-stitik <87721353+seth-stitik@users.noreply.github.com>
2024-05-13 21:17:38 -0300

    Update 20240513232231-create-hole.js
    'use strict' used twice

--- migrations/20240513232231-create-hole.js
+++ migrations/20240513232231-create-hole.js
@@ -1,7 +1,5 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
-'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('holes', {
