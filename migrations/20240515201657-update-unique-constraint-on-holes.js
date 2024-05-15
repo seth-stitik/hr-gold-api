42ed103fff6ee104d8c6e6b0e6a4d53e84c31565
seth-stitik <sstitik@gmail.com>
2024-05-15 17:40:03 -0300

    Update Migration file

--- migrations/20240515201657-update-unique-constraint-on-holes.js
+++ migrations/20240515201657-update-unique-constraint-on-holes.js
@@ -2,20 +2,102 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.removeConstraint('tee_boxes', 'tee_boxes_holeNumber_fkey');
+    await queryInterface.removeConstraint('tee_boxes', 'tee_boxes_courseID_fkey');
+    await queryInterface.removeConstraint('tee_boxes', 'tee_boxes_clubID_fkey');
+
     await queryInterface.removeConstraint('holes', 'holes_pkey');
+
     await queryInterface.addConstraint('holes', {
       fields: ['holeNumber', 'courseID'],
-      type: 'unique',
-      name: 'unique_hole_course'
+      type: 'primary key',
+      name: 'holes_pkey',
+    });
+
+    await queryInterface.addConstraint('tee_boxes', {
+      fields: ['holeNumber'],
+      type: 'foreign key',
+      name: 'tee_boxes_holeNumber_fkey',
+      references: {
+        table: 'holes',
+        field: 'holeNumber',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+
+    await queryInterface.addConstraint('tee_boxes', {
+      fields: ['courseID'],
+      type: 'foreign key',
+      name: 'tee_boxes_courseID_fkey',
+      references: {
+        table: 'courses',
+        field: 'courseID',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+
+    await queryInterface.addConstraint('tee_boxes', {
+      fields: ['clubID'],
+      type: 'foreign key',
+      name: 'tee_boxes_clubID_fkey',
+      references: {
+        table: 'golf_clubs',
+        field: 'clubID',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeConstraint('holes', 'unique_hole_course');
+    await queryInterface.removeConstraint('tee_boxes', 'tee_boxes_holeNumber_fkey');
+    await queryInterface.removeConstraint('tee_boxes', 'tee_boxes_courseID_fkey');
+    await queryInterface.removeConstraint('tee_boxes', 'tee_boxes_clubID_fkey');
+
+    await queryInterface.removeConstraint('holes', 'holes_pkey');
+
     await queryInterface.addConstraint('holes', {
       fields: ['holeNumber'],
-      type: 'unique',
-      name: 'holes_pkey'
+      type: 'primary key',
+      name: 'holes_pkey',
+    });
+
+    await queryInterface.addConstraint('tee_boxes', {
+      fields: ['holeNumber'],
+      type: 'foreign key',
+      name: 'tee_boxes_holeNumber_fkey',
+      references: {
+        table: 'holes',
+        field: 'holeNumber',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+
+    await queryInterface.addConstraint('tee_boxes', {
+      fields: ['courseID'],
+      type: 'foreign key',
+      name: 'tee_boxes_courseID_fkey',
+      references: {
+        table: 'courses',
+        field: 'courseID',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+
+    await queryInterface.addConstraint('tee_boxes', {
+      fields: ['clubID'],
+      type: 'foreign key',
+      name: 'tee_boxes_clubID_fkey',
+      references: {
+        table: 'golf_clubs',
+        field: 'clubID',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
     });
   }
 };
